26d764d19f37a62ac817cdd027b4b30f13b4a88a
LaughingBudda <subru@abc-pc.snu.in>
2019-09-03 10:39:53 +0530

    some unit testing

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -10,20 +10,20 @@ import (
 
 //Handler Functions
 func HandleHome(w http.ResponseWriter, r *http.Request) {
-    print("try to serve a home page")
+    w.Write([]byte("try to serve a home page"))
     w.WriteHeader(http.StatusOK)
     log.Print("Serving butterNote homepage")
 }
 
 func PostNote(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
-    w.WriteHeader(http.StatusOK)
     fmt.Fprintf(w, "Server got Note: %v\n", vars["note"])
     log.Printf("Server got Note: %v\n", vars["note"])
+    w.WriteHeader(http.StatusOK)
 }
 
 func GetNote(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Server replied with: na bhai na, abhi database nahi hai")
+    log.Printf("Server replied with: na bhai na, abhi database nahi hai")
     w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Server replied with: na bhai, abhi database nahi hai")
-    log.Printf("Server replied with: na bhai, abhi database nahi hai")
 }
--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -2,37 +2,28 @@ package handlers
 
 import (
 	"net/http"
-	"net/http/testing"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestHandleHome(t *testing.T) {
-    // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-    // pass 'nil' as the third parameter.
-    /*req, err := http.NewRequest("GET", "/health", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
+    // Create a request to pass to our handler
+    req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	//create the handler and send the request
+	handler := http.HandlerFunc(HandleHome)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
 
-    // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-    rr := httptest.NewRecorder()
-    handler := http.HandlerFunc(HealthCheckHandler)
-
-    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-    // directly and pass in our Request and ResponseRecorder.
-    handler.ServeHTTP(rr, req)
-
-    // Check the status code is what we expect.
-    if status := rr.Code; status != http.StatusOK {
-        t.Errorf("handler returned wrong status code: got %v want %v",
-            status, http.StatusOK)
-    }
-
-    // Check the response body is what we expect.
-    expected := `{"alive": true}`
+    // Check the response body is what we expect
+    expected := `try to serve a home page`
     if rr.Body.String() != expected {
-        t.Errorf("handler returned unexpected body: got %v want %v",
-            rr.Body.String(), expected)
-    }*/
+        t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+    }
 
 }
